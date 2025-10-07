5d759da9df2cc6ea7399c2d93f35e615e59568d5
ChristiaanTromp <c.c.trompje@gmail.com>
2025-10-07 11:46:00 +0200

    Update website dependencies and configuration

--- next.config.js
+++ next.config.js
@@ -4,10 +4,7 @@ const nextConfig = {
   swcMinify: true,
   compress: true,
   poweredByHeader: false,
-  // output: 'export', // Static export - uitgeschakeld voor development
-  // trailingSlash: true, // Voor betere hosting compatibiliteit - uitgeschakeld voor development
   images: {
-    // unoptimized: true, // Voor static export - uitgeschakeld voor development
     remotePatterns: [
       {
         protocol: 'https',
